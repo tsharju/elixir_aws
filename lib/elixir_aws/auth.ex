4a654deafef6aa0cec3b804c4d65762e6661e2f7
Teemu Harju <teemu.harju@gmail.com>
2015-03-13 16:03:49 +0200

    Signature V4 partially implemented.

---
+++ lib/elixir_aws/auth.ex
@@ -0,0 +1,51 @@
+defmodule Aws.Auth.Signature.V4 do
+
+  def sign(region, service, http_method, uri, headers, payload, query \\ "") do
+    digest(string_to_sign(region, service, http_method, uri, headers, payload, query))
+  end
+  
+  def string_to_sign(region, service, http_method, uri, headers, payload, query \\ "") do    
+    datetime = iso_8601_datetime()
+    [date, _] = String.split(datetime, "T")
+    
+    headers = [{"X-Amz-Date", date} | headers]
+    cr_string = canonical_request(http_method, uri, headers, payload, query)
+    hash = digest(cr_string)
+    
+    "AWS4-HMAC-SHA256\n#{datetime}\n#{date}/#{region}/#{service}/aws4_request\n#{hash}"
+  end
+
+  def canonical_request(http_method, uri, headers, payload, query \\ "") do
+    {signed_headers, canonical_headers} = canonical_headers(headers)
+    payload = digest(payload)
+    "#{http_method}\n#{uri}\n#{query}\n#{canonical_headers}\n#{signed_headers}\n#{payload}"
+  end
+  
+  def canonical_headers(headers) do
+    {signed_headers, canonical_headers} = Enum.map(headers,
+      fn {header, value} ->
+        {String.downcase(header), String.strip(value)}
+      end)
+    |> Enum.sort
+    |> Enum.map_reduce("",
+      fn ({header, value}, acc) ->
+        {header, acc <> "#{header}:#{value}\n"}
+      end)
+    {Enum.join(signed_headers, ";"), canonical_headers}
+  end
+
+  def digest(data) do
+    [digest] = :io_lib.format(
+      '~64.16.0b',
+      [:binary.decode_unsigned(:crypto.hash(:sha256, data))])
+    List.to_string(digest)
+  end
+
+  def iso_8601_datetime() do
+    {{year, month, day}, {hour, min, sec}} = :calendar.now_to_universal_time(:os.timestamp())
+    :lists.flatten(:io_lib.format('~4.10.0B~2.10.0B~2.10.0BT~2.10.0B~2.10.0B~2.10.0BZ',
+                                  [year, month, day, hour, min, sec]))
+    |> List.to_string
+  end
+  
+end
--- test/elixir_aws_test.exs
+++ test/elixir_aws_test.exs
@@ -2,16 +2,18 @@ defmodule AwsTest do
   use ExUnit.Case
   
   import Aws.Http
+
+  alias Aws.Auth.Signature
   
   test "Render URI template." do
     template = "/{Bucket}/{Key+}"
     assert "/bucket/key" =
-      render_uri_template(template, [bucket: "bucket",
-                                     key: "key"])
+      render_uri_template(template, [Bucket: "bucket",
+                                     Key: "key"])
     
     assert "/bucket%2Fbucket/key1/key2" =
-      render_uri_template(template, [bucket: "bucket/bucket",
-                                     key: "key1/key2"])
+      render_uri_template(template, [Bucket: "bucket/bucket",
+                                     Key: "key1/key2"])
   end
 
   test "Service endpoint config." do
@@ -31,6 +33,47 @@ defmodule AwsTest do
     assert shape.members[:Bucket].shape.type == "string"
     assert shape.members[:Key].shape.type == "string"
     assert shape.members[:UploadId].shape.type == "string"
+  end
+
+  test "Auth signature" do
+    http_method = "POST"
+    uri = "/"
+    headers = [{"Host", "iam.amazonaws.com"},
+               {"Content-Type", "application/x-www-form-urlencoded; charset=utf-8"}]
+    payload = "Action=ListUsers&Version=2010-05-08"
+    string_to_sign = Signature.V4.string_to_sign("us-east-1", "s3", http_method, uri, headers, payload)
+    
+    assert string_to_sign != nil # TODO: mock :os.timestamp so we can assert this
+  end
+
+  test "Canonical header string" do
+    headers = [{"host", "iam.amazonaws.com"},
+               {"Content-type", "application/x-www-form-urlencoded; charset=utf8"},
+               {"My-header1", "      a    b   c"},
+               {"x-amz-date", "20120228T030031Z"},
+               {"My-Header2", "a    b    c"}]
+
+    {signed_headers, canonical_string} = Signature.V4.canonical_headers(headers)
+
+    assert signed_headers == "content-type;host;my-header1;my-header2;x-amz-date"
+    
+    assert canonical_string == "content-type:application/x-www-form-urlencoded; charset=utf8\n" <>
+      "host:iam.amazonaws.com\nmy-header1:a    b   c\nmy-header2:a    b    c\n" <>
+      "x-amz-date:20120228T030031Z\n"
+  end
+
+  test "Canonical reguest" do
+    http_method = "POST"
+    uri = "/"
+    headers = [{"Host", "iam.amazonaws.com"},
+               {"Content-Type", "application/x-www-form-urlencoded; charset=utf-8"},
+               {"X-Amz-Date", "20110909T233600Z"}]
+    payload = "Action=ListUsers&Version=2010-05-08"
+    
+    canonical_request = Signature.V4.canonical_request(http_method, uri, headers, payload)
+    hash = Signature.V4.digest(canonical_request)
+    
+    assert hash == "3511de7e95d28ecd39e9513b642aee07e54f4941150d8df8bf94b328ef7e55e2"
   end
   
 end
